815cf527d7435b33b02b31a2f1aa040746e7eaad
Harshil Patel <drpatelh@users.noreply.github.com>
2023-09-18 11:29:21 +0200

    Update launch_hello_world.py

--- examples/python/launch_hello_world.py
+++ examples/python/launch_hello_world.py
@@ -1,32 +1,17 @@
 import logging
-
-# Import the twkit package
 from twkit import tower
 
-logging.basicConfig(level=logging.DEBUG)
-
-# Construct a new twkit Tower instance
 tw = tower.Tower()
 
-# Customise the entries below as required
-workspace = "<YOUR_WORKSPACE>"  # Name of your Workspace
-compute_env = "<YOUR_COMPUTE_ENVIRONMENT>"  # Name of your Compute Environment
-
-# Specify a human-readable run name
-run_name = "hello-world-twkit"
-
-# Launch the 'hello-world' pipeline using the 'launch' method
-pipeline_run = tw.launch(
+tw.launch(
     "--workspace",
-    workspace,
+    "twkit/showcase",
     "--compute-env",
-    compute_env,
+    "seqera_aws_ireland_fusionv2_nvme",
     "--name",
-    run_name,
+    "nf-core-rnaseq",
     "--revision",
-    "master",
-    "--wait",
-    "SUBMITTED",
-    "https://github.com/nextflow-io/hello",
-    to_json=True,
+    "3.12.0",
+    "https://github.com/nf-core/rnaseq",
+    to_json=True
 )
